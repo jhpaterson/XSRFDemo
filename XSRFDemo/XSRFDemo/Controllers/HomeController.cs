3a91759ede8db391ab032d0a303510daf701c112
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-31 21:39:47 +0000

    initial commit

---
+++ XSRFDemo/XSRFDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XSRFDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string username)
+        {
+            ViewBag.Message = "XSS Demo";
+            ViewBag.UserName = username;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // Use AccountController for XSRF and open redirection, account jim/Pa$$w0rd
+    }
+}
